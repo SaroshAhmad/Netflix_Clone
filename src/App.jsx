a17269cb88b8bc82f4182be328e27aa1a5b24fbe
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-10-25 17:34:59 +0100

    Add new assets and update app title along with favicon

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>React App</h1>
-      <p>Checking if my github is working</p>
-      <p>This is a para tag</p>
+      <h1>Netflix Clone</h1>
+      <p>React + Firebase</p>
     </div>
   )
 }
