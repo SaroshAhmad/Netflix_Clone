a8d69068e943e91fab3ae2b9cac4c7fdaff9eba2
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-10-28 16:51:58 +0000

    Refactor error code in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -30,7 +30,7 @@ const signUp = async (name, email, password) => {
         })
     } catch (error) {
         console.log(error);
-        toast.error(error.code);
+        toast.error(error.code.split('/')[1].split('-').join(' '));
     }
 }
 
@@ -39,7 +39,7 @@ const login = async (email, password) => {
         await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        toast.error(error.code);
+        toast.error(error.code.split('/')[1].split('-').join(' '));
     }
 }
 
